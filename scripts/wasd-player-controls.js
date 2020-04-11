abfb8856745b2239b3e4c14fb8a03232749b2706
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-04-11 21:13:34 +0200

    jumping is not working due to a-frame physics component bug

--- scripts/wasd-player-controls.js
+++ scripts/wasd-player-controls.js
@@ -1,7 +1,7 @@
 AFRAME.registerComponent("wasd-player-controls", {
     init: function() {
       window.addEventListener("keydown", (e) => {
-      const posVec = this.el.body.position;
+        const posVec = this.el.body.position;
         switch (e.keyCode) {
           case 38:
               this.el.body.position.set(posVec.x - 0.05, posVec.y, posVec.z);
@@ -14,6 +14,11 @@ AFRAME.registerComponent("wasd-player-controls", {
               break;
           case 40:
               this.el.body.position.set(posVec.x + 0.05, posVec.y, posVec.z);
+              break;
+          case 32: 
+              this.el.body.fixedRotation = true;
+              this.el.body.updateMassProperties();
+              this.el.body.applyImpulse(new CANNON.Vec3(0,50,0), this.el.body.position);
               break;
         }
       });
