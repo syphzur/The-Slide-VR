cc0bded06003b0013190c8430fd12a274a824e97
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-26 20:12:06 +0200

    arrows-controls fix

--- scripts/arrows-controls.js
+++ scripts/arrows-controls.js
@@ -1,19 +1,19 @@
 AFRAME.registerComponent("arrows-controls", {
     init: function() {
       window.addEventListener("keydown", (e) => {
-        const posVec = this.el.body.position;
+        const posVec = this.el.object3D.position;
         switch (e.keyCode) {
           case 38:
-              this.el.body.position.set(posVec.x - 0.05, posVec.y, posVec.z);
+              this.el.object3D.position.set(posVec.x - 0.05, posVec.y, posVec.z);
               break;
           case 37:
-              this.el.body.position.set(posVec.x, posVec.y, posVec.z + 0.05);
+              this.el.object3D.position.set(posVec.x, posVec.y, posVec.z + 0.05);
               break;
           case 39:
-              this.el.body.position.set(posVec.x, posVec.y, posVec.z - 0.05);
+              this.el.object3D.position.set(posVec.x, posVec.y, posVec.z - 0.05);
               break;
           case 40:
-              this.el.body.position.set(posVec.x + 0.05, posVec.y, posVec.z);
+              this.el.object3D.position.set(posVec.x + 0.05, posVec.y, posVec.z);
               break;
         }
       });
