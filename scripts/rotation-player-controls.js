17f7823889a7a04f33307bf77c2a35095abd1f01
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-26 20:01:31 +0200

    moving with head working again

--- scripts/rotation-player-controls.js
+++ scripts/rotation-player-controls.js
@@ -2,7 +2,8 @@ AFRAME.registerComponent("rotation-player-controls", {
     tick: function (time, timeDelta) {
       const rotation = this.el.object3D.rotation;
       const player = document.querySelector("#player");
-      const pos = player.body.position;
-      player.body.position.set(pos.x , pos.y, rotation.y * 7);
+      player.object3D.rotation.set(rotation.x, rotation.y, rotation.z);
+      const pos = player.object3D.position;
+      player.object3D.position.set(pos.x , pos.y, rotation.y * 7);
     }
   })
