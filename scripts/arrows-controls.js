11d1c4c691eda94624ca147c480b643a6446dcb2
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-05-26 15:11:11 +0200

    delete console logs

--- scripts/arrows-controls.js
+++ scripts/arrows-controls.js
@@ -5,19 +5,15 @@ AFRAME.registerComponent("arrows-controls", {
         switch (e.keyCode) {
           case 38:
               this.el.body.position.set(posVec.x - 0.05, posVec.y, posVec.z);
-              console.log(this.el.body.id);
               break;
           case 37:
               this.el.body.position.set(posVec.x, posVec.y, posVec.z + 0.05);
-              console.log(this.el.body.id);
               break;
           case 39:
               this.el.body.position.set(posVec.x, posVec.y, posVec.z - 0.05);
-              console.log(this.el.body.id);
               break;
           case 40:
               this.el.body.position.set(posVec.x + 0.05, posVec.y, posVec.z);
-              console.log(this.el.body.id);
               break;
         }
       });
