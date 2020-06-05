84886d39705c0b41ddb36b6ff2146972a6ea6545
Łukasz Latusik <latusikl@gmail.com>
2020-06-05 21:58:16 +0200

    Score changes.

--- scripts/components/collision.js
+++ scripts/components/collision.js
@@ -1,20 +1,21 @@
 //Sets collision event on player
+
 AFRAME.registerComponent("collision-detector", {
     init: function () {
         const player = this.el;
+        startScoreIncrement();
         player.addEventListener('collide', handleCollision);
     }
 })
 
-function handleCollision(collision){
+function handleCollision(collision) {
     const elem = collision.detail.body;
     console.log("colided with: ", elem);
-    
-    if(elem.el.className === 'bonus')
-    {
+
+    if (elem.el.className === 'bonus') {
         removeElementAndUpdateScore(elem.el);
     }
-    if(elem.el.className === "obstacle"){
+    if (elem.el.className === "obstacle") {
         endGame();
     }
     const playerCollider = document.querySelector("#player-collision").body;
@@ -31,21 +32,37 @@ function removeElementAndUpdateScore(element) {
     // hide or move the element
     // otherwise during deletion game crashes
     element.setAttribute("visible", "false");
-    updateScore();
+    updateScore(scoreUpdateValue.SCORE_BONUS_UPDATE);
 }
 
+function endGame() {
+    window.location.pathname = '/index.html'
+}
 
 //Score handling
 var score = 0;
-const textValueBeforeScore="value: Your score: ";
-const textValueAfterScore="; side: front; align: center; color: #941414";
-function updateScore(){
-    console.log('Score updated.');
-    var scoreElement = document.querySelector("#score");
-    score++;
-    scoreElement.setAttribute("text",textValueBeforeScore + score + textValueAfterScore);
+
+const TEXT_VALUE_BEFORE_SCORE = "value: Your score: ";
+const TEXT_VALUE_AFTER_SCORE = "; side: front; align: center; color: #941414";
+const SCORE_DISTANCE_UPDATE_INTERVAL = 300;
+
+const scoreUpdateValue = {
+    SCORE_BONUS_UPDATE: 250,
+    SCORE_DISTANCE_UPDATE: 1
+};
+
+function startScoreIncrement() {
+    console.log("inside");
+    setInterval(() => updateScore(scoreUpdateValue.SCORE_DISTANCE_UPDATE), SCORE_DISTANCE_UPDATE_INTERVAL);
 }
 
-function endGame(){
-    window.location.pathname = '/index.html'
+function updateScoreAfterBonusCollision() {
+    updateScore(scoreUpdateValue.SCORE_BONUS_UPDATE);
+}
+
+function updateScore(scoreUpdateValue) {
+    console.log('Score updated:' + scoreUpdateValue);
+    var scoreElement = document.querySelector("#score");
+    score += scoreUpdateValue;
+    scoreElement.setAttribute("text", TEXT_VALUE_BEFORE_SCORE + score + TEXT_VALUE_AFTER_SCORE);
 }
