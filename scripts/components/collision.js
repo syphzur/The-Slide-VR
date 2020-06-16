774f01b4263b4cf900589eb635c4a6a0890402e3
margawron <max2.marcel@gmail.com>
2020-06-16 19:38:28 +0200

    Add comments.

--- scripts/components/collision.js
+++ scripts/components/collision.js
@@ -1,39 +1,51 @@
-//Sets collision event on player
-
+//Sets collision event on entity having this component
 AFRAME.registerComponent("collision-detector", {
     init: function () {
         const player = this.el;
+        // Start adding points depending on time played
         startScoreIncrement();
         player.addEventListener('collide', handleCollision);
     }
 })
 
+// On collision detected
 let lastObstacleId = '';
 function handleCollision(collision) {
+    // Get body of element with which we have collided
     const elem = collision.detail.body;
     console.log("colided with: ", elem);
 
+    // Collided with bonus
     if (elem.el.className === 'bonus') {
         playBonusSound();
         removeElementAndUpdateScore(elem.el);
     }
+    // Collided with obstacle
     if (elem.el.className === "obstacle") {
         playCollisionSound();
+        // Workaround that allows player to pass through 
+        // obstacles and not lose all health
         if (lastObstacleId !== elem.el.id) {
-            removeLive()
+            removeLive() // Decrease health count
             if (lives < 1) {
                 endGame();
             }
             lastObstacleId = elem.el.id;
+            // Clear name of last hit obstacle
             setTimeout(() => lastObstacleId = '', 500);
         }
     }
+    // Get player collision body
     const playerCollider = document.querySelector("#player-collision").body;
+    // Get player body
     const player = document.querySelector("#player").body;
+    // Get player pos
     const pos = player.position;
+    // Clear collider forces
     clearForces(playerCollider);
     //! THIS SETS COLLIDER POSITION
     //! DESPITE DEBUG WIREFRAME SAYING OTHERWISE
+    // Move collider to player position 
     playerCollider.position.set(pos.x, pos.y, pos.z);
 }
 
@@ -45,10 +57,13 @@ function removeElementAndUpdateScore(element) {
     updateScore(scoreUpdateValue.SCORE_BONUS_UPDATE);
 }
 
+// Redirect player to end game page
 function endGame() {
-    localStorage.setItem('score', score);
+    localStorage.setItem('score', score); // Save score
+    // Workaround for github pages
     let href = location.href.substr(0, location.href.lastIndexOf('/'));
     href += "/index.html"
+    // Go to end game page
     location.href = href;
 }
 
@@ -58,34 +73,37 @@ let lives = 3;
 const TEXT_VALUE_BEFORE_SCORE = "value: Your score: ";
 const TEXT_VALUE_AFTER_SCORE = "; side: front; align: center; color: #941414";
 const SCORE_DISTANCE_UPDATE_INTERVAL = 1500;
-
+// Score constants
 const scoreUpdateValue = {
-    SCORE_BONUS_UPDATE: 250,
-    SCORE_DISTANCE_UPDATE: 1
+    SCORE_BONUS_UPDATE: 250, // Amount of points added per bonus
+    SCORE_DISTANCE_UPDATE: 1 // Amount of points added per single update
 };
 
+// Remove heart image 
 function removeLive() {
     if (lives === 3) {
-        document.getElementById('heart3').remove();
+        document.getElementById('heart3').remove(); // Remove right heart
     }
     else if (lives === 2) {
-        document.getElementById('heart2').remove();
+        document.getElementById('heart2').remove(); // Remove middle heart
     }
     else if (lives === 1) {
-        document.getElementById('heart1').remove();
+        document.getElementById('heart1').remove(); // Remove left heart
     }
-    lives--;
+    lives--; // Decrement value of lives
 }
 
-
+// Update score depending on how long game is played
 function startScoreIncrement() {
     setInterval(() => updateScore(scoreUpdateValue.SCORE_DISTANCE_UPDATE), SCORE_DISTANCE_UPDATE_INTERVAL);
 }
 
+// Update score after collision with bonus
 function updateScoreAfterBonusCollision() {
     updateScore(scoreUpdateValue.SCORE_BONUS_UPDATE);
 }
 
+// Add points to score and update text
 function updateScore(scoreUpdateValue) {
     var scoreElement = document.querySelector("#score");
     score += scoreUpdateValue;
--- scripts/sound.js
+++ scripts/sound.js
@@ -1,44 +1,43 @@
+// On document loaded
 $(document).ready(function() {
+    // Get saved volume
     let audioVolume = localStorage.getItem("volume");
-    
+    // If theres no saved volume
     if(!audioVolume) {
+        // Set default
         audioVolume = 0.5;
+        // Set value
         localStorage.setItem("volume",  audioVolume);
     }
-    
+    // Set slider value
     $("#volumeRange").val(audioVolume);
-
+    // Create HTML audio tag
     const audioEl = document.createElement("AUDIO");
+    // Append tag to <body>
     document.body.appendChild(audioEl);
-    audioEl.id = "audio";
-    audioEl.src = "audio/mainmenu.mp3"
-    audioEl.pause()
-    audioEl.setAttribute("muted", "muted")
+    audioEl.id = "audio";               // Set id
+    audioEl.src = "audio/mainmenu.mp3"  // Set src
+    audioEl.volume = audioVolume;       // Set volume
+    // Pause music for https://developers.google.com/web/updates/2017/09/autoplay-policy-changes
+    audioEl.pause() 
+    audioEl.setAttribute("muted", "muted") 
+    // Add listener for moving mouse and play background music
     document.body.addEventListener("mousemove", function(){
+        // Need to use promise
         const promise = audioEl.play();
         promise.then(_ => {
         }).catch(error => {
         })
     })
-
-    
-
-    const audio = document.getElementById("audio");
-    const volumeToSet = localStorage.getItem('volume');
-    
-    if (volumeToSet != null) {
-        audio.volume = volumeToSet;
-    }
-    else {
-        audio.volume = 0.5;
-    }
-
 })
 
+// On slider movement
 function changeVolume() {
+    // Get audio tag
     const audio = document.getElementById("audio");
+    // Change slider volume
     const newVolumeValue = document.getElementById("volumeRange").value;
-    audio.volume = newVolumeValue;
-    
+    audio.volume = newVolumeValue; // Change volume
+    // Save volume
     localStorage.setItem('volume', newVolumeValue);
 }
