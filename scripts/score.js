51f8d1d4c16517c134c08b5324e288a6cc9b00d8
Łukasz Latusik <latusikl@gmail.com>
2020-06-16 19:29:21 +0200

    Added comments in code.

--- scripts/score.js
+++ scripts/score.js
@@ -1,19 +1,21 @@
+//Script is responsible for showing result and playing sound after end of game.
+//Every 0.1 second check if site loading is complete. Remove interval after dociument is ready.
 let stateCheck = setInterval(() => {
     if (document.readyState === 'complete') {
-        checkIfReloadedAfterGameEnd();
+        showResult();
         clearInterval(stateCheck);
+        playGameOverSound();
     }
 }, 100);
-
-function checkIfReloadedAfterGameEnd() {
+//Function injects html code which will show 
+function showResult() {
     if (localStorage.getItem('score')) {
         document.getElementById('gameOver').innerHTML = '<h1>GAME OVER!</h1>' +
             '<h2>Your result: ' + localStorage.getItem('score') + '</h2>';
         localStorage.removeItem('score');
-        playGameOverSound();
     }
 }
-
+//Function plays "game over" music if not muted.
 function playGameOverSound() {
     const sound = document.getElementById('gameoverSound');
     const volume = localStorage.getItem('volume');
