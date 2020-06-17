845a140403f0e7ec7693019870db26ff827f733e
Krzysztof Ból <krzysztof.bol@wp.pl>
2020-06-17 12:24:32 +0200

    game over sound fix

--- scripts/score.js
+++ scripts/score.js
@@ -1,18 +1,18 @@
 //Script is responsible for showing result and playing sound after end of game.
-//Every 0.1 second check if site loading is complete. Remove interval after dociument is ready.
+//Every 0.1 second check if site loading is complete. Remove interval after document is ready.
 let stateCheck = setInterval(() => {
     if (document.readyState === 'complete') {
         showResult();
         clearInterval(stateCheck);
-        playGameOverSound();
     }
 }, 100);
-//Function injects html code which will show 
+//Function injects html code which will show result and play game over sound.
 function showResult() {
     if (localStorage.getItem('score')) {
         document.getElementById('gameOver').innerHTML = '<h1>GAME OVER!</h1>' +
             '<h2>Your result: ' + localStorage.getItem('score') + '</h2>';
         localStorage.removeItem('score');
+        playGameOverSound();
     }
 }
 //Function plays "game over" music if not muted.
